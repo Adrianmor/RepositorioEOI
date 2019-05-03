f8bfe76b340c352f261afb7ce6e96874132a76ac
admin <admin@ALC-45T9LQ1.usersad.everis.int>
2019-05-03 16:51:54 +0200

    commit

---
+++ Test/src/com/es/eoi/Buscaminas.java
@@ -0,0 +1,27 @@
+package com.es.eoi;
+
+public class Buscaminas {
+
+	public static void main(String[] args) {
+		
+
+		
+		int altura=10;
+		int anchura=10;
+		
+		int[][]tablero= new int [altura][anchura];
+		
+		System.out.println(tablero);
+		
+		for (int i = 0; i < tablero.length; i++) {
+			int[] js = tablero[i];
+			
+			System.out.println(js);
+			
+		}
+		
+				
+
+	}
+
+}
---
+++ Test/src/com/es/eoi/Cube.java
@@ -0,0 +1,5 @@
+package com.es.eoi;
+
+public class Cube {
+
+}
